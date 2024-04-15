82f5c2e992b38598392b9f08de8f61c35f8b993e
lucarl07 <junitinho2007@gmail.com>
2024-04-15 12:28:25 -0300

    Stylize the modal containers

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,24 @@
+// Components and stylesheets:
+import React from 'react';
 import './Button.css'
+
+// Images:
+import iconMinus from '../../assets/images/icon-minus.svg';
+import iconPlus from '../../assets/images/icon-plus.svg';
+
+const Icon = ({modal}) => {
+    const image = modal === true? iconMinus : iconPlus
+
+    return (
+        <img src={image} alt="Click to enable or disable" />
+    );
+}
 
 const Button = ({setModal, modal, text}) => {
     return (
         <button className='btn' onClick={() => setModal(!modal)}>
-            {text}
+            <span>{text}</span>
+            <Icon modal={modal} />
         </button>
     );
 }
--- src/components/ModalContainer/ModalContainer.jsx
+++ src/components/ModalContainer/ModalContainer.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Button from "../Button/Button";
 import Modal from "../Modal/Modal";
+import './ModalContainer.css';
 
 const ModalContainer = ({question, answer}) => {
     const [modal, setModal] = React.useState(false);
 
     return (
         <div className='question'>
-            <Button setModal={setModal} modal={modal} text={question}/>
+            <Button setModal={setModal} modal={modal} text={question} />
             <Modal modal={modal} text={answer}/>
+            <hr/>
         </div>
     );
 }
